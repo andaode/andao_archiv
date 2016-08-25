7f2d87a0a4b54ef1c82f447daf23d2c54931f016
Andy <info@andao.de>
2016-08-25 23:49:58 +0200

    override

--- ocr/archive.py
+++ ocr/archive.py
@@ -4,9 +4,9 @@ from openerp import osv, models, fields, api, tools, _
 import base64
 import datetime
 import logging
-#import tesseract
-
-#import cv2.cv as cv
+from PIL import Image as dupa
+from pytesser import *
+import cStringIO as StringIO
 _logger = logging.getLogger(__name__)
 
 
@@ -17,12 +17,13 @@ class Document(models.Model):
     _name = 'ocr.document'
     _inherit = 'mail.thread'
     _description = 'Archive document'
-
+    _order = 'scanned_on desc'
     name = fields.Char(string='Dokument ID')
     doc_name = fields.Char(string='Dokument')
     file_upload = fields.Binary(string='Datei', attachment=True)
+    image_fname = fields.Char(string='Dateiname')
     image_small = fields.Binary(string='Bild', attachment=True, readonly=True)
-    image_medium = fields.Binary(string='Bild', attachment=True, readonly=True)
+    #image_medium = fields.Binary(string='Bild', attachment=True, readonly=True)
     image_big = fields.Binary(string='Bild', attachment=True, readonly=True)
 
     doc_text = fields.Text()
@@ -34,8 +35,8 @@ class Document(models.Model):
 
     state = fields.Selection([('terminated', 'Verschoben'),('open', 'Nicht gelesen'), ('closed', 'Gelesen'), ('not_approved', 'Nicht bestätigt')
                               ], 'Status')
-    def _resize_image(self):
-        self.image = tools.image_resize_image_medium(self.image)
+    # def _resize_image(self):
+    #     self.image = tools.image_resize_image_medium(self.image)
 
     @api.multi
     @api.depends('reminder')
@@ -64,33 +65,29 @@ class Document(models.Model):
         self.env.cr.execute('SELECT count(name) FROM ocr_document WHERE owner = %s', (vals['owner'],))
         amount = str(self.env.cr.fetchall()[0][0] or 0)
         new_number = int(amount) + 1
-        vals['name'] = 'Dokumen ' + str(new_number)
+        vals['name'] = 'Dokument ' + str(new_number)
+        #image_stream = StringIO.StringIO(vals['file_upload'].decode('base64'))
         vals['image_small'] = tools.image_resize_image_medium(vals['file_upload'])
-        vals['image_medium'] = tools.image_resize_image_small(vals['file_upload'])
+        #vals['image_medium'] = tools.image_resize_image_small(vals['file_upload'])
         vals['image_big'] = tools.image_resize_image_big(vals['file_upload'])
         vals['is_not_read'] = True
-        if not vals['scanned_on'] :
+        if not vals['scanned_on']:
             vals['scanned_on'] = str(datetime.datetime.now())[:10]
         if not vals['received_on']:
             vals['received_on'] = str(datetime.datetime.now())[:10]
-
-        vals['image_big'] = tools.image_resize_image_big(vals['file_upload'])
         vals['state'] = 'not_approved'
-        #self.ocr_file(file)
+
+        #image_file = '/home/andy/odoo-8.0/pytesser/phototest.tif'
+        ocr_text = self.ocr_file(vals['file_upload'])
+        vals['doc_text'] = ocr_text
         if not vals['doc_name']:
             vals['doc_name'] = 'Dokument ' + str(new_number)
         agr = super(Document, self).create(vals)
         return agr
 
-    # def ocr_file(self, file):
-    #     mImgFile = "/Users/andrzej/projects/Octomore/ocr/data/0002AA72.jpg"
-    #     api = tesseract.TessBaseAPI()
-    #     api.SetOutputName("outputName");
-    #     api.Init(".","eng",tesseract.OEM_DEFAULT)
-    #     api.SetPageSegMode(tesseract.PSM_AUTO)
-    #     #mImgFile = "eurotext.jpg"
-    #     pixImage=cv.LoadImage(mImgFile)
-    #     tesseract.SetCvImage(pixImage, api)
-    #     outText=api.GetUTF8Text()
-    #     print("OCR output:\n%s"%outText);
-    #     api.End()
+    def ocr_file(self, file_binary):
+        image_stream = StringIO.StringIO(file_binary.decode('base64'))
+        #im = dupa.open('/home/andy/' + file_name)
+        im = dupa.open(image_stream)
+        text = pytesser.image_to_string(im)
+        return text
